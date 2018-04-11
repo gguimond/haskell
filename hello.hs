1e15d51c210efa8bd0854b100063105f653c72f5
Guillaume Guimond <guimog@gmail.com>
2018-04-11 09:49:14 +0200

    recursion

--- hello.hs
+++ hello.hs
@@ -1,4 +1,5 @@
 -- http://learn.hfm.io/index.html
+import Data.Char 
 
 echo = putStrLn "Hello, World!"
 mylength s = length (s)
@@ -16,4 +17,119 @@ mymax2 :: Ord a => a -> a -> a
 mymax2 x y | x >= y = x
 			| y > x = y
 t2 = mymax 3 4
-main = putStrLn(show t2) 
+main5 = putStrLn(show t2) 
+
+natSum :: (Num a, Ord a) => a -> a
+natSum 0              = 0
+natSum n  | n > 0     = n + natSum (n - 1) 
+          | otherwise = error "natSum: Input value too small!"
+main6 = putStrLn(show(natSum(3)))
+
+repeatN :: Int -> a -> [a]
+repeatN 0 x  = []
+repeatN n x  = x : repeatN (n - 1) x
+a = repeatN 3 5
+main7 = putStrLn(show(a!!0))
+
+suffixes :: String -> [String]
+suffixes ""  = []
+suffixes str = str : suffixes (tail str)
+b = suffixes "TOto"
+main8 = putStrLn(show(b!!2))
+
+allSquares :: Num a => [a] -> [a]
+allSquares []       = []
+allSquares (x : xs) = x * x : allSquares xs
+c = allSquares [1,2,3]
+main9 = putStrLn(show(c!!2))
+
+
+
+allToUpper :: String -> String
+allToUpper []                 = []
+allToUpper (chr : restString) = toUpper chr : allToUpper restString
+d = allToUpper "tedt"
+main10 = putStrLn(d)
+
+extractDigits :: String -> String
+extractDigits []
+  = []
+extractDigits (chr : restString)
+  | isDigit chr = chr : extractDigits restString
+  | otherwise   =       extractDigits restString
+e = extractDigits "1tedt"  
+main11 = putStrLn(show(e!!0))
+
+myproduct :: Num a => [a] -> a
+myproduct []     = 1
+myproduct (x:xs) = x * myproduct xs
+f = myproduct [1,2,3]
+main12 = putStrLn(show(f))
+
+(+++) :: [a] -> [a] -> [a]
+[]     +++ ys = ys
+(x:xs) +++ ys = x : (xs ++ ys)
+g = [1,2,3] +++ [5]
+main13 = putStrLn(show(g!!3))
+
+myreverse :: [a] -> [a]
+myreverse []     = []
+myreverse (x:xs) = myreverse xs ++ [x]
+h = myreverse "Hello"
+main14 = putStrLn(h)
+
+deductFromAccount balance []
+  = balance
+deductFromAccount balance (d : ds)
+  | balance < d = error ("Your account balance is " ++ show balance ++
+                         " - cannot deduct " ++ show d ++ " cents")
+  | otherwise   = deductFromAccount (balance - d) ds
+i = deductFromAccount 75645 [322, 434, 5343, 234]
+main15 = putStrLn(show i)
+
+stringToInt :: String -> Int
+stringToInt str = stringToIntAcc 0 str
+	where
+    stringToIntAcc :: Int -> String -> Int
+    stringToIntAcc acc []
+       = acc
+    stringToIntAcc acc (chr : restString) 
+       = stringToIntAcc (10 * acc + digitToInt chr) restString
+j = stringToInt "12345"
+main16 = putStrLn(show j)
+
+
+fastReverse :: [a] -> [a]
+fastReverse xs = reverseAcc [] xs
+  where
+    reverseAcc :: [a] -> [a] -> [a]
+    reverseAcc accList []     = accList
+    reverseAcc accList (x:xs) = reverseAcc (x : accList) xs
+k = fastReverse [1,2,3,4,5]
+main17 = putStrLn(show(k!!0))
+
+
+sumEvenELems :: Integral a => [a] -> a
+sumEvenELems xs = sum (filterEven xs)
+	where 
+		filterEven :: Integral b => [b] -> [b]
+		filterEven [] = []
+		filterEven (x:xs)
+			| even x = x : filterEven xs
+			| otherwise = filterEven xs
+l = sumEvenELems [1,2,3,4,5]
+main18 = putStrLn(show(l))
+
+sumOfSquareRoots xs = sum (allSquareRoots (filterPositives xs))
+  where
+    allSquareRoots []     = []
+    allSquareRoots (x:xs) = sqrt x : allSquareRoots xs
+
+    filterPositives [] 
+      = []
+    filterPositives (x:xs)
+      | x > 0     = x : filterPositives xs
+      | otherwise = filterPositives xs 
+m = sumOfSquareRoots [1,2,3,4,5]
+main = putStrLn(show(m))                           
+
